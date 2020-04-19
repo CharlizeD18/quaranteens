87f46058d685452120ff472f88b7b30e63acdd19
Charlize Dang <charlized18@berkeley.edu>
2020-04-19 11:34:18 -0700

    First Commit

---
+++ quaranteens/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  quaranteens
+//
+//  Created by Charlize Dang on 2020-04-19.
+//  Copyright © 2020 Charlize Dang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
