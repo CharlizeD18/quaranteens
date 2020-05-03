0e164be38af8943a80d8807b47495b3d7de90fb9
Charlize Dang <charlized18@berkeley.edu>
2020-05-03 00:47:10 -0700

    adding Google login

--- Quaranteen Project/Quaranteen Project/AppDelegate.swift
+++ Quaranteen Project/Quaranteen Project/AppDelegate.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import GoogleSignIn
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
+    
 
     var window: UIWindow?
 
